cf0f889092bc6b50657fd8b07415f52392f19607
dogol45-cyber <clarioleon45@gmail.com>
2026-09-24 11:44:08 -0700

    Update inscription.js

--- functions/api/inscription.js
+++ functions/api/inscription.js
@@ -1,11 +1,8 @@
-export async function onRequestPost(context) {
+export async function onRequestPost({ request, env }) {
 
-  const { request, env } = context;
+  const d = await request.json();
 
-  const data = await request.json();
-
-  const numero =
-    "MEH-" + Date.now();
+  const numero = "MEH-" + Date.now();
 
   await env.DB.prepare(`
     INSERT INTO inscriptions (
@@ -15,30 +12,26 @@ export async function onRequestPost(context) {
       tel_parent, option_formation,
       photo_piece, date_inscription
     )
-
-    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
   `).bind(
-
     numero,
-    data.nom,
-    data.prenom,
-    data.sexe,
-    data.naissance,
-    data.lieu,
-    data.adresse,
-    data.telephone,
-    data.email,
-    data.parent,
-    data.tel_parent,
-    data.option,
-    data.photo_piece,
+    d.nom,
+    d.prenom,
+    d.sexe,
+    d.naissance,
+    d.lieu,
+    d.adresse,
+    d.telephone,
+    d.email,
+    d.parent,
+    d.tel_parent,
+    d.option,
+    d.photo_piece,
     new Date().toISOString()
-
   ).run();
 
   return Response.json({
-    success:true,
+    success: true,
     numero
   });
 
